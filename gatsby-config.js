2c898d465f6b9ef4f65bc72d743e143ad5f3d7e7
Matt Nelson <matthewnelson18@gmail.com>
2020-02-04 16:45:13 -0800

    intro to graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Level Up Blog`,
+    description: `A tutorial for Gatsby so I can do this ish quickly and more efficiently`,
+    author: `Matty Ice`,
+    hola: `mundo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ notes/05-into-graph-ql.md
@@ -0,0 +1,5 @@
+Working with the data layer with graphql
+
+Graphql takes a shit on fetch
+
+Hover and click on the stuff in \_\_\_graphql and I can see what the
